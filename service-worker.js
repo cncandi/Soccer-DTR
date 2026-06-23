8759ead04b808cc1ef5ac8066759b38b129f4cad
Andreas Reitz <reitz@cnc-technik.de>
2026-06-23 22:36:31 +0200

    Add tactic board name scaling and orientation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soccer-dtr-v99";
+const CACHE_NAME = "soccer-dtr-v100";
 const BADGE_DB_NAME = "soccer-dtr-badges";
 const BADGE_STORE_NAME = "counts";
 const MESSAGE_BADGE_KEY = "messages";
@@ -6,10 +6,10 @@ const APP_SHELL = [
   "./",
   "index.html",
   "backend.html",
-  "taktikboard-3d.html?v=99",
-  "css/app.css?v=99",
+  "taktikboard-3d.html?v=100",
+  "css/app.css?v=100",
   "css/backend.css?v=1",
-  "js/app.js?v=99",
+  "js/app.js?v=100",
   "js/backend.js?v=4",
   "manifest.webmanifest",
   "assets/kadrivo-login-banner.jpg",
