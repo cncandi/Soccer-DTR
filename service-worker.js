3ff0bfe8a3ace7693f17a003bbb0db0723e3aa73
Andreas Reitz <andreas@cnc-technik.de>
2026-06-30 19:46:16 +0000

    v244: Typ + Taktikboard-Felder aus Übungs-Modal entfernt (immer tactic intern)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soccer-dtr-v243";
+const CACHE_NAME = "soccer-dtr-v244";
 const BADGE_DB_NAME = "soccer-dtr-badges";
 const BADGE_STORE_NAME = "counts";
 const MESSAGE_BADGE_KEY = "messages";
